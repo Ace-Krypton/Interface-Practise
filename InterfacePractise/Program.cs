678ea9c7374fd656b652462e78a0725f44092646
Ace-Krypton <ramizna@code.edu.az>
2021-06-24 22:25:17 +0400

    Add project files.

---
+++ InterfacePractise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        //Interface
+        interface IEatable
+        {
+            void Eat();
+        }
+
+        //Interface
+        interface IRunnable
+        {
+            void Run();
+        }
+
+        class Human : IRunnable
+        {
+            public void Run()
+            {
+                Console.WriteLine("Run as Human");
+            }
+        }
+
+        class Animal : IRunnable, 
+        {
+            public void Run()
+            {
+                Console.WriteLine("Run as Animal");
+            }
+        }
+
+    }
+}
