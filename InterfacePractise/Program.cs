fff71670923bafb71d5c78addd254303d9aab504
Ace-Krypton <ramizna@code.edu.az>
2021-06-24 22:27:50 +0400

    commit1

--- InterfacePractise/Program.cs
+++ InterfacePractise/Program.cs
@@ -14,15 +14,21 @@ namespace InterfacePractise
         }
 
         //Interface
-        interface IEatable
+        interface IEatable //Interfaces do not have method bodies like abstract classes, But in C# 8 and higher versions, you can add method body in your interface.
         {
             void Eat();
         }
 
         //Interface
+        interface Sum //Interfaces do not have method bodies like abstract classes, But in C# 8 and higher versions, you can add method body in your interface.
+        {
+            int Sum();
+        }
+
+        //Interface
         interface IRunnable
         {
-            void Run();
+            void Run(); //
         }
 
         class Human : IRunnable
